5acce1e33adf59f0f865a451cd6e93dc1a39bb5e
Yomna Elnady <yomna.a.elnady@gmail.com>
2019-12-26 02:17:05 +0000

    before edit and delete

---
+++ Sahaab site/Controllers/CustomersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sahaab_site.Models;
+
+namespace Sahaab_site.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Display()
+        {
+            var customer = new Customer(){Name="John doe"};
+            return View(customer);
+        }
+    }
+}
---
+++ Sahaab site/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sahaab_site.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public List<int> PhoneNumbers { get; set; }
+
+    }
+}
